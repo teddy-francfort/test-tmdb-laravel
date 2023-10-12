d1149e4d0cf1c07c419bb01508a9320fc6f2651e
Teddy FRANCFORT <teddy.francfort@gmail.com>
2023-10-12 10:23:39 +0200

    :children_crossing: Add confirmation modal when deleting movie

--- resources/views/livewire/movies/show-movie.blade.php
+++ resources/views/livewire/movies/show-movie.blade.php
@@ -38,6 +38,7 @@
                         <button
                             type="button"
                             wire:click="delete"
+                            wire:confirm="Are you sure you want to delete this movie ?"
                             class='inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150'
                         >
                             {{ __('Delete !') }}
